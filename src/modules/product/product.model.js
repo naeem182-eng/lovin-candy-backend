0959647ad078ceef9cbd24a42cdcde4cb40f56c0
seriouspcy <seriouspcy@gmail.com>
2026-01-14 17:56:11 +0700

    cart product

---
+++ src/modules/product/product.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    description: {
+      type: String,
+      trim: true
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    stock: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    images: [
+      {
+        type: String 
+      }
+    ],
+
+    isActive: {
+      type: Boolean,
+      default: true
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model("Product", productSchema);
