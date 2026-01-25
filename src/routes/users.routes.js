2171f67d91d0919185cd83c6c437667501a3737d
JackJack2405 <chutikankin7442@gmail.com>
2026-01-25 12:32:31 +0700

    added role and action in suer

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,35 +1,33 @@
 import { Router } from "express";
-import { createAddress, createUser, delAddress, getUsers, login, me, register, updateAddress, updateProfile, deleteUser } from "../modules/users/users.controller.js";
+import {
+  createUser,
+  getUsers,
+  login,
+  me,
+  register,
+  updateAddress,
+  updateProfile,
+  deleteUser,
+  createAddress,
+  delAddress,
+} from "../modules/users/users.controller.js";
+
 import { auth } from "../middlewares/auth.middleware.js";
 import { isAdmin } from "../middlewares/isAdmin.middleware.js";
 
 export const router = Router();
 
 router.post("/login", login);
-
 router.post("/register", register);
 
 router.get("/me", auth, me);
 
 router.post("/address", auth, createAddress);
-
 router.put("/update-address", auth, updateAddress);
-
-router.get("/", getUsers);
-
-router.post("/", createUser);
-
+router.delete("/address", auth, delAddress);
 
 router.put("/update-profile", auth, updateProfile);
 
-
-router.delete("/address", auth, delAddress);
-
-
 router.get("/", auth, isAdmin, getUsers);
-
-router.delete("/:id", auth, isAdmin, delAddress);
-
-router.get("/", auth, isAdmin, getUsers);
-
+router.post("/", auth, isAdmin, createUser);
 router.delete("/:id", auth, isAdmin, deleteUser);
