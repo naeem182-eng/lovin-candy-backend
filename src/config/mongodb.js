047c1741b0302838051b21627980413a07b09316
naeem182 <naeem182@gmail.com>
2026-01-31 00:09:43 +0700

    change config DB cnd add .env.test

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,10 +2,15 @@ import mongoose from "mongoose";
 
 export async function connectDB() {
   const uri = process.env.MONGODB_URI;
+  const dbName = process.env.MONGODB_DB_NAME;
+
+  if (!uri || !dbName) {
+    throw new Error("Missing MongoDB environment variables");
+  }
 
   try {
-    await mongoose.connect(uri, { dbName: "lovin-candy" });
-    console.log("MongoDB connected");
+    await mongoose.connect(uri, { dbName });
+    console.log(`MongoDB connected → ${dbName}`);
   } catch (error) {
     console.error("MongoDB connection error", error);
     process.exit(1);
